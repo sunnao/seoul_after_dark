642307b516f7cb8599db290dac8e2fb67d824417
sunnao <osa125125@naver.com>
2025-03-18 21:22:04 +0900

    html parser 추가

--- src/features/map/components/DetailPlaceContext.tsx
+++ src/features/map/components/DetailPlaceContext.tsx
@@ -1,26 +1,50 @@
-import { ViewNightSpot } from "@/features/map/types/mapTypes";
+import { ViewNightSpot } from '@/features/map/types/mapTypes';
+import parse from 'html-react-parser';
 
 interface DetailPlaceProps {
   selectedPlace: ViewNightSpot;
 }
 
-const DetailPlaceContext = ({selectedPlace}: DetailPlaceProps) => {
+const DetailPlaceContext = ({ selectedPlace }: DetailPlaceProps) => {
   return (
-    <div className="text-start">
-      <h4 className="mb-2 text-xl font-bold">{selectedPlace.TITLE}</h4>
-      <p className="mb-2 text-gray-400">{selectedPlace.ADDR}</p>
-      {selectedPlace.TEL_NO && <p className="mb-2 text-gray-400">전화: {selectedPlace.TEL_NO}</p>}
-      {selectedPlace.URL && (
-        <p className="mb-2 truncate text-blue-500">
-          <span className="mb-2 text-gray-400">홈페이지: </span>
-          <a href={selectedPlace.URL} target="_blank" rel="noopener noreferrer">
-            {selectedPlace.URL}
-          </a>
+    <>
+      <div className="text-end">
+        <p className="mb-2 text-xs text-gray-400">
+          업데이트: {selectedPlace.MOD_DATE.slice(0, 16)}
         </p>
-      )}
-      {/* <p className="mt-4 text-gray-700">{selectedPlace.CONTENTS}</p> */}
-    </div>
+      </div>
+      <div className="text-start">
+        <h4 className="mb-2 text-xl font-bold">{selectedPlace.TITLE}</h4>
+        <p className="mb-2 text-gray-400">{selectedPlace.ADDR}</p>
+        {selectedPlace.OPERATING_TIME && (
+          <p className="mb-2 text-gray-300">
+            운영시간: <span className="mb-2 text-gray-400">{selectedPlace.OPERATING_TIME}</span>
+          </p>
+        )}
+        {selectedPlace.TEL_NO && (
+          <p className="mb-2 text-gray-300">
+            전화: <span className="mb-2 text-gray-400">{selectedPlace.TEL_NO}</span>
+          </p>
+        )}
+        {selectedPlace.URL && (
+          <p className="mb-2 text-gray-300">
+            홈페이지:{' '}
+            <a
+              className="break-all text-blue-500"
+              href={selectedPlace.URL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {selectedPlace.URL}
+            </a>
+          </p>
+        )}
+        {selectedPlace.CONTENTS && (
+          <p className="mt-4 text-gray-400">{parse(selectedPlace.CONTENTS)}</p>
+        )}
+      </div>
+    </>
   );
 };
 
-export default DetailPlaceContext
+export default DetailPlaceContext;
