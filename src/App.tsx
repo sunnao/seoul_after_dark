f519ef1c2b8b6f4ee04558a10dd6cd6dae3fa27e
sunnao <osa125125@naver.com>
2025-03-20 07:47:54 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Map } from './features/map/components/Map';
+import { LoginPage } from '@/pages/LoginPage';
+import { Layout } from '@/components/layouts/Layout';
+import { MapPage } from '@/pages/MapPage';
+import { MapLayout } from '@/components/layouts/MapLayout';
 
 function App() {
   return (
-    <>
-      <Map />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<MapLayout />}>
+          {/* 일반 페이지들 - 스크롤 가능한 콘텐츠 */}
+          <Route index element={<MapPage />} />
+        </Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="favorite" element={<MapPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
