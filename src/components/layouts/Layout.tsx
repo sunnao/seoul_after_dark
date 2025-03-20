b37f03311a1c26715548d0ddf226bd12401fcf66
sunnao <osa125125@naver.com>
2025-03-20 10:37:09 +0900

    ..

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from '@/components/layouts/Header';
-import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
