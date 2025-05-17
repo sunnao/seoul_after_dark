d6af541b63d3cd4f03868d8be04c91c1c7c8da9d
sunnao <osa125125@naver.com>
2025-05-18 01:05:02 +0900

    fix: 카카오 로그인 중복 호출 수정

--- src/features/auth/components/KakaoLogin.tsx
+++ src/features/auth/components/KakaoLogin.tsx
@@ -2,25 +2,31 @@ import { useSearchParams } from 'react-router-dom';
 import { useCallback, useEffect } from 'react';
 import { useAuth } from '@/features/auth/hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
 
 export const KakaoLogin = () => {
   const { kakaoLogin } = useAuth();
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const code = searchParams.get('code');
+  const hasCalled = useRef(false);
 
   const handleKakaoToken = useCallback(async () => {
+    if (hasCalled.current) {
+      return;
+    }
+    hasCalled.current = true;
+
     if (code) {
       const kakaoAuthResult = await kakaoLogin(code);
 
       if (kakaoAuthResult) {
         navigate('/');
-      }
+      } 
     }
-  }, [code, kakaoLogin, navigate]);
+  }, [code]);
 
   useEffect(() => {
-    console.log('code==', code);
     if (code !== null) {
       handleKakaoToken();
     }
